d01f74a8fcb6a94b8ee13da83244f63033cfae5c
Shusan <shusantandukar@gmail.com>
2020-08-02 09:36:51 +0545

    sign in controller added

--- app/src/main/java/com/example/bnn_news/Activity/Login_Activity.java
+++ app/src/main/java/com/example/bnn_news/Activity/Login_Activity.java
@@ -10,11 +10,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.bnn_news.APIs.UserApi;
+import com.example.bnn_news.BLL.UserBLL;
 import com.example.bnn_news.MainActivity;
+import com.example.bnn_news.Models.UserModel;
 import com.example.bnn_news.R;
+import com.example.bnn_news.Utlis.Helpers;
 
 public class Login_Activity extends AppCompatActivity {
     EditText email,password;
+    private UserBLL userBLL;
     Button btnlogin;
     TextView register;
 
@@ -22,6 +27,7 @@ public class Login_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        userBLL=new UserBLL();
         email=findViewById(R.id.emaillog);
         password=findViewById(R.id.password);
         btnlogin=findViewById(R.id.btnlogin);
@@ -34,7 +40,25 @@ public class Login_Activity extends AppCompatActivity {
             }
         });
 
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signIn();
+            }
+        });
+    }
+    private void signIn(){
+        Helpers.StrictMode();
+        String emaillog=email.getText().toString();
+        String passwordlog=password.getText().toString();
+        UserModel authUser=userBLL.loginUser(emaillog,passwordlog);
+        if(authUser!=null){
+            Intent intent=new Intent(Login_Activity.this, MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, "Login Successfull", Toast.LENGTH_SHORT).show();
+        }else {
+            Toast.makeText(this, "Login Failed", Toast.LENGTH_SHORT).show();
+        }
 
     }
-
 }
